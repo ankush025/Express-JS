e1a7ccdd2df24c8c6c02fbe4487600f132645219
ankush25 <ankushthummar25@gmail.com>
2024-05-20 09:35:32 +0530

    Middleware Use / 20-05-2024

--- server.js
+++ server.js
@@ -1,20 +1,69 @@
-// const http = require('http');
-// const server = http.createServer().listen(1234);    // create Server
-
-
 const express = require("express");
 const server = express();
+const morgan = require('morgan');
 
-server.get("/",(req,res)=>{                       // Data Get
+
+
+
+// MiddleWare
+
+// server.use((req,res,next) => {
+//     console.log(req.method,req.get('User -agent'));     // log
+//     next();
+// });
+
+let auth = (req,res,next) => {
+    // console.log(req.query);           // { name: 'ankush', age: '18', surname: 'Thummar' }
+
+    // if(req.query.age >= 18){
+    //     next();
+    // }else{
+    //     res.send('Sorry,your age Smallest ....!!!!');
+    // }
+
+
+
+    // console.log(req.body);
+    if(req.body.age >= 18){
+            next();
+        }else{
+            res.send('Sorry,your age Smallest ....!!!!');
+        }
+    
+}
+
+
+// server.use(auth);                                 // Application Level
+server.use(express.json());                          // boody under row json data write
+server.use(express.urlencoded({extended:true}));     // urlencoded 
+
+
+
+
+server.get("/", auth ,(req,res)=>{                       
     res.send("Welcome to Express Server");
 })
 
-server.get("/hello",(req,res)=>{
-    res.send("<h1>Hello</h1>");
+server.post("/", auth ,(req,res)=> {                    
+    res.json({type: 'Post Methd'});
 })
+ 
+server.put("/",(req,res)=> {                    
+    res.json({type: 'Put Methd'});
+})
+
+server.patch("/",(req,res)=> {               
+    res.json({type: 'Patch Methd'});
+})
+ 
+server.delete("/",(req,res)=> {                
+    res.json({type: 'Delete Methd'});
+})
+
+
 
 
 
 server.listen(1122,()=> {
-    console.log('Server Start....');
+    console.log('Server Start......');
 });
