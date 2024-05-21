c780afd3f9134d77a66c9d8cd34819d3405d6be2
ankush25 <ankushthummar25@gmail.com>
2024-05-21 09:25:10 +0530

    Middleware - express.static / 21-05-2024

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const server = express();
 const morgan = require('morgan');
+const path = require('path');
+const filepath = path.join(__dirname,'Public');
 
 
 
 
 // MiddleWare
 
-// server.use((req,res,next) => {
-//     console.log(req.method,req.get('User -agent'));     // log
-//     next();
-// });
 
 let auth = (req,res,next) => {
     // console.log(req.query);           // { name: 'ankush', age: '18', surname: 'Thummar' }
@@ -24,41 +22,41 @@ let auth = (req,res,next) => {
 
 
     // console.log(req.body);
-    if(req.body.age >= 18){
-            next();
-        }else{
-            res.send('Sorry,your age Smallest ....!!!!');
-        }
+    // if(req.body.age >= 18){
+    //         next();
+    //     }else{
+    //         res.send('Sorry,your age Smallest ....!!!!');
+    //     }
+
+
+    if(req.body){
+        next();
+    }
     
 }
 
 
 // server.use(auth);                                 // Application Level
-server.use(express.json());                          // boody under row json data write
-server.use(express.urlencoded({extended:true}));     // urlencoded 
+server.use(express.static(filepath));                // Express Statis
+server.use(express.json());                          // Express JSON
+server.use(express.urlencoded({extended:true}));     // Express urlencoded 
 
 
 
 
 server.get("/", auth ,(req,res)=>{                       
+    res.send("Hello");
+})
+
+server.get("/data", auth ,(req,res)=>{                       
+    res.sendFile(`${filepath}/data.json`);
+})
+
+server.get("/1", auth ,(req,res)=>{                       
     res.send("Welcome to Express Server");
 })
 
-server.post("/", auth ,(req,res)=> {                    
-    res.json({type: 'Post Methd'});
-})
- 
-server.put("/",(req,res)=> {                    
-    res.json({type: 'Put Methd'});
-})
 
-server.patch("/",(req,res)=> {               
-    res.json({type: 'Patch Methd'});
-})
- 
-server.delete("/",(req,res)=> {                
-    res.json({type: 'Delete Methd'});
-})
 
 
 
