d8230c1ed22adc5f82af3097e0d899209e4b03bf
ankush25 <ankushthummar25@gmail.com>
2024-05-23 09:20:58 +0530

    Basic APIs - PUT-PATCH_DELETE / 23-05-2024

--- server.js
+++ server.js
@@ -35,7 +35,51 @@ server.get('/product/:id',(req,res)=>{
     // console.log(typeof(id));
     const item = products.find((e)=> e.id === id)
     res.status(200).json(item);
-})
+});
+
+
+
+
+
+
+
+
+
+
+// Replace => PUT Method
+server.put('/products/:id', (req,res)=>{
+    const id = +req.params.id;
+    // console.log(typeof(id));
+    const itemindex = products.findIndex((e)=> e.id === id)
+    // const product = products[itemindex];
+
+    products.splice(itemindex , 1 , {...req.body});
+    res.status(200).json({message : 'Product is Replaced...'})
+});
+
+
+// Update => PATCH Method
+server.patch('/products/:id', (req,res)=>{
+    const id = +req.params.id;
+    // console.log(typeof(id));
+    const itemIndex = products.findIndex((e)=> e.id === id)
+    const product = products[itemIndex];
+
+    products.splice(itemIndex , 1 , {...product, ...req.body});
+    res.status(200).json({message : 'Product is Updated...'})
+});
+
+
+// Delete => DELETE Method
+server.delete('/products/:id', (req,res)=>{
+    const id = +req.params.id;
+    // console.log(typeof(id));
+    const itemindex = products.findIndex((e)=> e.id === id)
+    // const product = products[itemindex];
+
+    products.splice(itemindex , 1);
+    res.status(200).json({message : 'Product is Deleted...'})
+});
 
 
 
