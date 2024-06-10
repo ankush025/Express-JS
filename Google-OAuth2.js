3c915f8bda4caefef4100401b46f18c40dcb768e
ankush25 <ankushthummar25@gmail.com>
2024-06-10 16:07:39 +0530

    Work / 10-06-2024

--- Google-OAuth2.js
+++ Google-OAuth2.js
@@ -1,61 +1,63 @@
-// Cart
+// Google OAuth2
+
 const express = require("express");
-const mongoose = require("mongoose");             // Mongoose require
+const path = require('path');
+const app = express();
+require('./passportJS')
 const session = require('express-session');
 const passport = require('passport');
 
-const cartRoutes = require('./routes/cart.routes');
-server.use("/api/carts",cartRoutes);
+app.use(express.static(path.join(__dirname,'client')));
 
 function isLoggedIn(req,res,next){
     req.user ? next() : res.sendStatus(401);
 }
 
-server.get('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.sendFile('index.html');
 });
 
-server.use(session({
+app.use(session({
     secret: 'mysecret',
     resave: false,
     saveUnitialized: true,
     cookie: { secure: false}
 }));
 
-server.use(passport.initialize()); 
-server.use(passport.session());
+app.use(passport.initialize()); 
+app.use(passport.session());
 
 // Passport Google oauth-2
 const auth = require('./passportJS');
 
-server.get('/auth/google',
+app.get('/auth/google',
     passport.authenticate("google",{ scope:
         ['email','profile']
     })
 );
 
-server.get('/auth/google/callback',
+app.get('/auth/google/callback',
     passport.authenticate('google',{
         successRedirect: '/auth/protected',
         failureRedirect: '/auth/google/failure'
     })
 );
 
-server.get('/auth/google/failure',(req,res)=>{
+app.get('/auth/google/failure',(req,res)=>{
     res.send("Something went wrong!");
 })
 
 
-server.get('/auth/protected',isLoggedIn,(req,res)=>{
+app.get('/auth/protected',isLoggedIn,(req,res)=>{
     let name = req.user.displayName;
-    res.send(`Hello $(name)`);
+    res.send(`Hello ${name}`);
 })
 
-server.use('/auth/logout',(req,res)=>{
+app.use('/auth/logout',(req,res)=>{
     req.session.destroy();
     res.send('See You Again...!!!')
 })
 
-server.listen(9000,()=> {
+app.listen(5000,()=> {
     console.log('Server Start......');
 });
--- passportJS.js
+++ passportJS.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+require('dotenv').config()
 
 passport.use(new GoogleStrategy({
     clientID:     process.env.GOOGLE_CLIENT_ID,
